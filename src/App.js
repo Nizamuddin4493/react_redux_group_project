64c77076d7c908bc1d14c60f5f55decd6ff391f5
rejoice <rmjspecial2015@gmail.com>
2022-08-30 14:58:33 +0100

    add index.js index.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Missions from './components/Missions';
+import MyProfile from './components/MyProfile';
+import Rockets from './components/Rockets';
+import './App.css';
+
 function App() {
   return (
-    <>
-      <h1>Hello Nizam</h1>
-    </>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/myprofile" element={<MyProfile />} />
+        <Route path="/" element={<Rockets />} />
+      </Routes>
+    </div>
   );
 }
 
