b22ddc760ecf6ba11bedeb0ece34fc415813ef0f
rejoice <rmjspecial2015@gmail.com>
2022-08-31 09:29:12 +0100

    modified app.js

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,48 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import planet from './images/planet.png'
+import planet from './images/planet.png';
 
-const Header = () => {
-  
-  return(
-    <>
-      <div className="headContainer">
-        <div name="logo">
-          <img src={ planet } alt="Planet" />
-          <h1>Space Travelers' Hub</h1>
-        </div>
-        <div name="links">
-          <ul>
-            <li><NavLink to="/" className={({ isActive }) =>
-                isActive ? 'active' : 'reactLink'
-              }>Rockets</NavLink></li>
-            <li><NavLink to="/missions" className={({ isActive }) =>
-                isActive ? 'active' : 'reactLink'
-              }>Missions</NavLink></li>
-            <li>|</li>
-            <li><NavLink to="/myProfile" className={({ isActive }) =>
-                isActive ? 'active' : 'reactLink'
-              }>My Profile</NavLink></li>
-          </ul>
-        </div>
+const Header = () => (
+  <>
+    <div className="headContainer">
+      <div name="logo">
+        <img src={planet} alt="Planet" />
+        <h1>Space Travelers Hub</h1>
       </div>
-    </>
-  );
-}
+      <div name="links">
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              className={
+                ({ isActive }) => (isActive ? 'active' : 'reactLink')
+                }
+            >
+              Rockets
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/missions"
+              className={({ isActive }) => (isActive ? 'active' : 'reactLink')}
+            >
+              Missions
+            </NavLink>
+          </li>
+          <li>|</li>
+          <li>
+            <NavLink
+              to="/myProfile"
+              className={({ isActive }) => (isActive ? 'active' : 'reactLink')}
+            >
+              My Profile
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </>
+);
 
 export default Header;
