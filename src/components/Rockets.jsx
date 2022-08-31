5557f94e84fd73206865c2d1d1dadd4606b92fd8
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-08-31 15:10:34 +0430

    setup redux directory and connect read redux

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import test from './images/FB_IMG_1534950152549.jpg'
+import test from './images/FB_IMG_1534950152549.jpg';
 
 const Rockets = () => (
   <>
@@ -9,8 +9,8 @@ const Rockets = () => (
       </div>
       <div name="info">
         <div><h2>Falcon 1</h2></div>
-        <div><p> ekjkfejhf;ekhf;ek fekljf ;ekhf;ekjfh ;keh;f jhe; fkjeh; fkje;f kje;f kje ;fklje ;fkje ;fej ;flejf;lkej f;lejdf ekjkfejhf;ekhf;ek fekljf ;ekhf;ekjfh ;keh;f jhe; fkjeh; fkje;f kje;f kje ;fklje ;fkje ;fej ;flejf;lkej f;lejdf ekjkfejhf;ekhf;ek fekljf ;ekhf;ekjfh ;keh;f jhe; fkjeh; fkje;f kje;f kje ;fklje ;fkje ;fej ;flejf;lkej f;lejdf ekjkfejhf;ekhf;ek fekljf ;ekhf;ekjfh ;keh;f jhe; fkjeh; fkje;f kje;f kje ;fklje ;fkje ;fej ;flejf;lkej f;lejdf ekjkfejhf;ekhf;ek fekljf ;ekhf;ekjfh ;keh;f jhe; fkjeh; fkje;f kje;f kje ;fklje ;fkje ;fej ;flejf;lkej f;lejdf</p></div>
-        <div><button>Reserve Rocket</button></div>
+        <div><p> ekjkfejhf;ekhf;ek fekljf ;ekhf;ekjfh ;keh;f jhe; fkjeh; fkje;f kje;f kje </p></div>
+        <div><button type="button">Reserve Rocket</button></div>
       </div>
     </div>
 
@@ -20,8 +20,8 @@ const Rockets = () => (
       </div>
       <div name="info">
         <div><h2>Falcon 1</h2></div>
-        <div><p> ekjkfejhf;ekhf;ek fekljf ;ekhf;ekjfh ;keh;f jhe; fkjeh; fkje;f kje;f kje ;fklje ;fkje ;fej ;flejf;lkej f;lejdf ekjkfejhf;ekhf;ek fekljf ;ekhf;ekjfh ;keh;f jhe; fkjeh; fkje;f kje;f kje ;fklje ;fkje ;fej ;flejf;lkej f;lejdf ekjkfejhf;ekhf;ek fekljf ;ekhf;ekjfh ;keh;f jhe; fkjeh; fkje;f kje;f kje ;fklje ;fkje ;fej ;flejf;lkej f;lejdf ekjkfejhf;ekhf;ek fekljf ;ekhf;ekjfh ;keh;f jhe; fkjeh; fkje;f kje;f kje ;fklje ;fkje ;fej ;flejf;lkej f;lejdf ekjkfejhf;ekhf;ek fekljf ;ekhf;ekjfh ;keh;f jhe; fkjeh; fkje;f kje;f kje ;fklje ;fkje ;fej ;flejf;lkej f;lejdf</p></div>
-        <div><button>Reserve Rocket</button></div>
+        <div><p> ekjkfejhf;ekhf;ek fekljf ;ekhf;ekjfh ;keh;f jhe; fkjeh; fkje;f kje;f kje</p></div>
+        <div><button type="button">Reserve Rocket</button></div>
       </div>
     </div>
 
@@ -31,8 +31,8 @@ const Rockets = () => (
       </div>
       <div name="info">
         <div><h2>Falcon 1</h2></div>
-        <div><p> ekjkfejhf;ekhf;ek fekljf ;ekhf;ekjfh ;keh;f jhe; fkjeh; fkje;f kje;f kje ;fklje ;fkje ;fej ;flejf;lkej f;lejdf ekjkfejhf;ekhf;ek fekljf ;ekhf;ekjfh ;keh;f jhe; fkjeh; fkje;f kje;f kje ;fklje ;fkje ;fej ;flejf;lkej f;lejdf ekjkfejhf;ekhf;ek fekljf ;ekhf;ekjfh ;keh;f jhe; fkjeh; fkje;f kje;f kje ;fklje ;fkje ;fej ;flejf;lkej f;lejdf ekjkfejhf;ekhf;ek fekljf ;ekhf;ekjfh ;keh;f jhe; fkjeh; fkje;f kje;f kje ;fklje ;fkje ;fej ;flejf;lkej f;lejdf ekjkfejhf;ekhf;ek fekljf ;ekhf;ekjfh ;keh;f jhe; fkjeh; fkje;f kje;f kje ;fklje ;fkje ;fej ;flejf;lkej f;lejdf</p></div>
-        <div><button>Reserve Rocket</button></div>
+        <div><p> ekjkfejhf;ekhf;ek fekljf ;ekhf;ekjfh ;keh;f jhe; fkjeh; fkje;f kje;f </p></div>
+        <div><button type="button">Reserve Rocket</button></div>
       </div>
     </div>
   </>
