d538f017414709cdd00dee23247f09c936fd8ba0
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-09-01 21:47:13 +0430

    useSelector is used from redux hooks

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,30 +1,38 @@
-import React from 'react';
-import test from './images/FB_IMG_1534950152549.jpg';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { rockets } from '../redux/rockets/rockets';
 
-const Rockets = () => (
-  <>
-    <div className="missionConatiner">
-      <div>
-        <img src={test} alt="Mission" />
+const Rockets = () => {
+  const store = useSelector((state) => state.rocketReducer);
+  console.log(store);
+  useEffect(() => {
+    rockets();
+  }, [store]);
+  return (
+    <>
+      <div className="missionConatiner">
+        <div>
+          <img src={test} alt="Mission" />
+        </div>
+        <div name="info">
+          <div><h2>Falcon 1</h2></div>
+          <div><p> ekjkfejhf;ekhf;ek fekljf ;ekhf;ekjfh ;keh;f</p></div>
+          <div><button type="button">Reserve Rocket</button></div>
+        </div>
       </div>
-      <div name="info">
-        <div><h2>Falcon 1</h2></div>
-        <div><p> ekjkfejhf;ekhf;ek fekljf ;ekhf;ekjfh ;keh;f</p></div>
-        <div><button type="button">Reserve Rocket</button></div>
-      </div>
-    </div>
 
-    <div className="missionConatiner">
-      <div>
-        <img src={test} alt="Mission" />
+      <div className="missionConatiner">
+        <div>
+          <img src={test} alt="Mission" />
+        </div>
+        <div name="info">
+          <div><h2>Falcon 1</h2></div>
+          <div><p> ekjkfejhf;ekhf;ek fekljf ;ekhf;ekjfh ;keh;f jhe; fkjeh; fkje;f</p></div>
+          <div><button type="button">Reserve Rocket</button></div>
+        </div>
       </div>
-      <div name="info">
-        <div><h2>Falcon 1</h2></div>
-        <div><p> ekjkfejhf;ekhf;ek fekljf ;ekhf;ekjfh ;keh;f jhe; fkjeh; fkje;f</p></div>
-        <div><button type="button">Reserve Rocket</button></div>
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default Rockets;
