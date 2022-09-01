15ebfd5b50d38b4909cc5a70ba0dd1190ba2d969
rejoice <rmjspecial2015@gmail.com>
2022-09-01 19:52:22 +0100

    display-mission-list

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { missionThunk } from '../redux/missions/mission';
 
 function Missions() {
+  const missions = useSelector((state) => state.missionReducer);
+  useEffect(() => {
+    missionThunk();
+  }, [missions]);
   return (
-    <div>
-      <div>
-        <h2>hjjjkk</h2>
-        <p>treggh</p>
-      </div>
-    </div>
+    <>
+      <table>
+        <tbody>
+          <tr>
+            <th style={{ width: '12%' }}>Mission</th>
+            <th style={{ width: '60%' }}>Description</th>
+            <th>Status</th>
+            <th>---</th>
+          </tr>
+          { missions.map((data) => (
+            <tr key={data.mission_id}>
+              <td className="missionTitle">{data.mission_name}</td>
+              <td>{data.description}</td>
+              <td><button type="button" className="notAMemberBtn">NOT A MEMBER</button></td>
+              <td><button type="button" className="joinMemberBtn">Join Mission</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
