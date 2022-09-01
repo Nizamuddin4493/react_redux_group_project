b84e9af3bbe25e919f08728dd1d0afd40462b57c
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-09-01 21:40:20 +0430

    Fetching data is successfull.

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,13 @@
-import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { combineReducers, applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
-import rocketReducer from './rockets/rockets';
+import rocketReducer, { rockets } from './rockets/rockets';
 import missions from './missions/mission';
 
 const rootReducer = combineReducers({ rocketReducer, missions });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = configureStore({ reducer: rootReducer }, applyMiddleware(thunk));
+
+store.dispatch(rockets());
 
 export default store;
