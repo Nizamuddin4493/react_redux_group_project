79acbf8368731973b6215d097ad83b34a3faf6cd
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-09-03 13:19:29 +0430

    remove some linters errors

--- src/__tests__/Missions.test.js
+++ src/__tests__/Missions.test.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
 import store from '../redux/configureStore';
