f8ee766a01f7ef4e3b66c3b14d07eaaf13f74325
rejoice <rmjspecial2015@gmail.com>
2022-09-03 08:16:19 +0100

    create a display resevation

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,27 +1,38 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const MyProfile = () => (
-  <>
-    <div className="profileContainer">
-      <div className="missionSection">
-        <h1>My Missions</h1>
-        <div name="missonList">
-          <li>Some text goes her</li>
-          <li>Some text goes her</li>
-          <li>Some text goes her</li>
-          <li>Some text goes her</li>
+const MyProfile = () => {
+  const missionStore = useSelector((state) => state.missionReducer);
+  const reservedMissions = missionStore.filter((mission) => mission.reserved === true);
+
+  const rocketStore = useSelector((state) => state.rocketReducer);
+  const reservedRocket = rocketStore.filter((mission) => mission.reserved === true);
+
+  return (
+    <>
+      <div className="profileContainer">
+        <div className="missionSection">
+          <h1>My Missions</h1>
+          <div name="missonList">
+            { reservedMissions.map((mission) => (
+              <li key={mission.mission_id}>
+                {mission.mission_name}
+              </li>
+            ))}
+          </div>
+        </div>
+        <div className="rocketSection">
+          <h1>My Rockets</h1>
+          <div name="rocketList">
+            { reservedRocket.map((rocket) => (
+              <li key={rocket.id}>
+                {rocket.rocket_name}
+              </li>
+            ))}
+          </div>
         </div>
       </div>
-      <div className="rocketSection">
-        <h1>My Rockets</h1>
-        <div name="rocketList">
-          <li>Some text goes her</li>
-          <li>Some text goes her</li>
-          <li>Some text goes her</li>
-        </div>
-      </div>
-    </div>
-  </>
-);
-
+    </>
+  );
+};
 export default MyProfile;
